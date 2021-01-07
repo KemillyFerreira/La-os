59491845cd5ca58bca15af04db598cd8ec09a05a
Kemilly Ferreira <73208400+KemillyFerreira@users.noreply.github.com>
2021-01-07 01:15:38 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Laços
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Aprendendo WHILE");
+         
+         Console.WriteLine("Digite um número");
+         int numero = int.Parse(Console.ReadLine());
+
+         int contador = 0;
+
+         while (contador <= numero)
+         {
+         Console.WriteLine(contador);
+         contador = contador +1;
+         };
+
+         string resposta = "";
+         do{
+         Console.WriteLine("Digite seu e-mail");
+         resposta = Console.ReadLine();
+
+         }while(resposta != "exgorfo@gmail.com");
+
+         Console.WriteLine("E-mail confirmado");
+              
+        Console.WriteLine("Digite a idade do ator");
+        int idade = int.Parse(Console.ReadLine());
+
+        while(idade != 47)
+        {
+           Console.WriteLine("Errou!");
+           Console.WriteLine("Digite a idade do ator novamente!");
+           idade = int.Parse(Console.ReadLine());
+        }   
+           Console.WriteLine("Acertou!");
+
+        int idade;
+        do{
+           Console.WriteLine("Digite a idade do ator");
+           idade = int.Parse(Console.ReadLine());
+        }while(idade != 47);
+        Console.WriteLine("Acertou!");
+
+        for(var contador = 0; contador < 10; contador++)
+        {
+           Console.WriteLine("Digite um valor");
+           int valor = int.Parse(Console.ReadLine());
+           Console.WriteLine($"Você digitou: {valor}");
+        }
+    }
+}
+}
